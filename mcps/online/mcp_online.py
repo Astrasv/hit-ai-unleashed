12ed57cff3ea6ca62955f87895e4f6d59373718d
Astrasv <sudharsan.vanamali@gmail.com>
2026-05-08 13:33:56 +0530

    Session 2: Hosted MCPs

---
+++ mcps/online/mcp_online.py
@@ -0,0 +1,59 @@
+import asyncio
+import os
+import sys
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Use the stable path for the prebuilt agent
+
+
+from langchain.agents import create_agent
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+
+async def main():
+    server_script = os.path.abspath(
+        "D:/A_Projects/A_ Git hosted/ai_unleashed_hit/mcps/local/mcp_server.py"
+    )
+
+    client = MultiServerMCPClient(
+        {"kiwi-com-flight-search": {"url": "https://mcp.kiwi.com", "transport": "http"}}
+    )
+
+    # Furniture example for future reference
+    # client = MultiServerMCPClient(
+    #     {{"furniture-hub": {"url": "https://ai-furniture-hub.onrender.com/mcp"}}}
+    # )
+
+    async with client.session("kiwi-com-flight-search") as session:
+
+        tools = await client.get_tools()
+
+        model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+        agent = create_agent(model=model, tools=tools)
+
+        response = await agent.ainvoke(
+            {
+                "messages": [
+                    HumanMessage(
+                        content="Show me all flight tickets from coimbatore to london which is affordable and has good reviews On sept 19th 2026"
+                    )
+                ]
+            }
+        )
+        print("\nFinal Response:")
+        print(response["messages"][-1].content)
+
+
+if __name__ == "__main__":
+
+    # Standard boilerplate for Windows loop issues
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(main())
