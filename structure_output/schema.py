245daf37b5110ac43cc59ceff4dd34db2c17710d
Astrasv <sudharsan.vanamali@gmail.com>
2026-08-07 16:35:11 +0530

    Session 4: Schema fix

--- structure_output/schema.py
+++ structure_output/schema.py
@@ -2,7 +2,9 @@ from pydantic import BaseModel, Field
 
 class WonderResponse(BaseModel):
     name: str = Field(...)
-    rating: float = Field(ge=0.0, le=5.0)
+    rating: float = Field(le=0.0, ge=5.0)
+    #  Show different field params
+    
     description: str = Field(...)
     coutnry: str = Field(...)
 
